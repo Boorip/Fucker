6c07ed1015c503f0e09cf256f2de2672535d65bf
Boorip <209272077+Boorip@users.noreply.github.com>
2025-04-28 14:12:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,6 +37,8 @@ SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+rFrVYWJbaCsxNzRl")
 
 API_URL = getenv("API_URL", 'https://api.thequickearn.xyz')
 API_KEY = getenv("API_KEY", None)
+COOKIE_URL = getenv("COOKIE_URL")
+
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
